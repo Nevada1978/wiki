749855e2f64bb07e9a6da5798f53f776d93b421e
ziye <pgmmmglg@outlook.com>
2024-07-03 16:10:51 +0800

    2-2 增加端口输出日志

--- src/main/java/com/ziye/wiki/WikiApplication.java
+++ src/main/java/com/ziye/wiki/WikiApplication.java
@@ -1,13 +1,28 @@
 package com.ziye.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
-	}
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env;
+
+		// 应该先运行app，然后获取Environment对象
+		ConfigurableApplicationContext context = app.run(args);
+		env = context.getEnvironment();
+
+		// 将日志记录移到环境变量访问之后，并且放在正确的代码块内
+		LOG.info("启动成功");
+		LOG.info("地址: http://127.0.0.1:{}/", env.getProperty("server.port"));
+
+	}
 }
