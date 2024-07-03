eec0debd261cce167659d5cd1cdf14e586b679b8
ziye <pgmmmglg@outlook.com>
2024-07-03 17:04:25 +0800

    2-4 修改启动界面，增加热部署配置--pom.xml

--- src/main/java/com/ziye/wiki/controller/TestController.java
+++ src/main/java/com/ziye/wiki/controller/TestController.java
@@ -1,17 +1,26 @@
 package com.ziye.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
-//返回字符串
+import java.util.Map;
+
 @RestController
-//返回页面
-//@Controller
 public class TestController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello world!";
+
+    //搜索配置文件中的test.hello
+    @Value("${test.hello:Test}")
+    private String testHello;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!"+testHello;
     }
 
+    @PostMapping("/hello/post")
+    public String helloPost(@RequestParam Map<String, String> map) {
+        // 假设你想获取名为"name"的参数，需要确保map中有这个键，否则会抛出异常
+        String nameParam = map.getOrDefault("name", "Name not provided");
+        return "Hello world post!!!" + nameParam;
+    }
 }
